77c13689ad1c23da47a46ae6ce42928dd9da919b
Giovane Timm <giovane.timm@sambatech.com.br>
2022-02-20 14:34:19 -0300

    fix: Fixed error 'BUFFER' in database connect and others improvements and refactoring did

--- src/app.js
+++ src/app.js
@@ -1,64 +1,15 @@
-const fixtures = require('../src/fixtures');
-const footballRespository = require('../src/footballRespository');
+const db = require('../src/db');
+const fixtureService = require('../src/services/fixtureService');
 
-async function getLiveFixtures() {
-    try {
-        const response = await fixtures.getCurrentLiveFixtures();
-        return response.data.response;
-    } catch (err) {
-        console.log(`error in getLiveFixtures: ${err}`);
-    }
-}
+//conect and start application
+db.connect()
+    .then(() => console.log('database connected'))
+    .then(() => {
+        fixtureService.syncFixturesStatsyncLiveFixturesAndRealTimeStatsToDatabasesToDatabase();
+        setInterval(fixtureService.syncLiveFixturesAndRealTimeStatsToDatabase, 1000 * 60);
+    })
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    });
 
-function syncFixturesInDataBase(fixtures) {
-    try {
-        fixtures.map(async (liveFixture) => {
-            try {
-                liveFixture.id = liveFixture.fixture.id;
-                await footballRespository.replaceObj({ id: liveFixture.fixture.id }, { $set: liveFixture }, 'fixtures');
-            } catch (err) {
-                console.log(`error in syncFixturesInDataBase: ${err}`);
-            }
-
-        });
-    } catch (err) {
-        console.log(`error in syncFixturesInDataBase: ${err}`);
-    }
-
-}
-
-async function syncFixturesStatsInDataBase(fixture) {
-    try {
-        console.log(`sincing stats from fixture id ${fixture.id}`);
-        const timeResponse = new Date();
-        const response = await fixtures.getFixturesLiveStats(fixture.id);
-
-        let fixtureStats = response.data;
-        fixtureStats.time = timeResponse;
-        fixtureStats.fixture_id = fixture.id;
-
-        footballRespository.insertObj(fixtureStats, 'fixture_realtime_stats');
-    } catch (err) {
-        console.log(`error in syncFixturesStatsInDataBase: ${err}`);
-    }
-}
-
-
-async function syncFixturesStatsToDatabase() {
-    try {
-        console.log('sincing stats from football api...');
-
-        let liveFixtures = await getLiveFixtures();
-        syncFixturesInDataBase(liveFixtures);
-
-        liveFixtures.map(async (fixture) => {
-            syncFixturesStatsInDataBase(fixture);
-        });
-    } catch (err) {
-        console.log(`error in syncFixturesStatsToDatabase: ${err}`);
-    }
-}
-
-syncFixturesStatsToDatabase();
-
-setInterval(syncFixturesStatsToDatabase, 1000 * 60);
